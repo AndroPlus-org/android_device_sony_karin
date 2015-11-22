20a238624d99cd1374cf916fddd95b415fc798a9
WedyDQ10 <hosigumayuugi@gmail.com>
2015-11-22 19:26:14 +0900

    Update touch won't work when system is not mounted. I'm trying to fix but didn't work...

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,8 +26,38 @@ TW_CUSTOM_CPU_TEMP_PATH := /sys/class/thermal/thermal_zone10/temp
 
 TW_THEME := landscape_hdpi
 
-TARGET_SCREEN_HEIGHT := 2560
-TARGET_SCREEN_WIDTH := 1600
 DEVICE_RESOLUTION := 2560x1600
 
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
+
+# TS config from kitakami-common
+PRODUCT_COPY_FILES += \
+    device/sony/kitakami/rootdir/system/usr/keylayout/touch_fusion.kl:recovery/root/system/usr/keylayout/touch_fusion.kl \
+    device/sony/kitakami/rootdir/system/usr/idc/touch_fusion.idc:recovery/root/system/usr/idc/touch_fusion.idc
+
+# TS config
+PRODUCT_COPY_FILES += \
+    device/sony/karin/rootdir/system/vendor/etc/touch_fusion_panel_id_0x0000.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0000.cfg \
+    device/sony/karin/rootdir/system/vendor/etc/touch_fusion_panel_id_0x0700.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0700.cfg \
+    device/sony/karin/rootdir/system/vendor/etc/touch_fusion_panel_id_0x0701.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0701.cfg \
+    device/sony/karin/rootdir/system/vendor/etc/touch_fusion_panel_id_0x0702.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0702.cfg \
+    device/sony/karin/rootdir/system/vendor/etc/touch_fusion_panel_id_0x0704.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0704.cfg \
+    device/sony/karin/rootdir/system/vendor/etc/touch_fusion_panel_id_0x0705.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0705.cfg \
+    device/sony/karin/rootdir/system/vendor/etc/touch_fusion_panel_id_0x0706.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0706.cfg \
+    device/sony/karin/rootdir/system/vendor/etc/touch_fusion_panel_id_0x0708.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0708.cfg \
+    device/sony/karin/rootdir/system/vendor/etc/touch_fusion_panel_id_0x0709.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0709.cfg \
+    device/sony/karin/rootdir/system/vendor/etc/touch_fusion_panel_id_0x070A.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x070A.cfg \
+    device/sony/karin/rootdir/system/vendor/etc/touch_fusion_panel_id_0x070C.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x070C.cfg \
+    device/sony/karin/rootdir/system/vendor/etc/touch_fusion_panel_id_0x070D.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x070D.cfg \
+    device/sony/karin/rootdir/system/vendor/etc/touch_fusion_panel_id_0x0710.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0710.cfg \
+    device/sony/karin/rootdir/system/vendor/etc/touch_fusion_panel_id_0x0717.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x0717.cfg \
+    device/sony/karin/rootdir/system/vendor/etc/touch_fusion_panel_id_0x071B.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x071B.cfg \
+    device/sony/karin/rootdir/system/vendor/etc/touch_fusion_panel_id_0x071F.cfg:recovery/root/system/vendor/etc/touch_fusion_panel_id_0x071F.cfg
+
+# TS FW loader
+PRODUCT_COPY_FILES += \
+    vendor/sony/karin/proprietary/vendor/bin/touch_fusion:recovery/root/system/vendor/bin/touch_fusion
+
+# TS FW
+PRODUCT_COPY_FILES += \
+    vendor/sony/karin/proprietary/vendor/firmware/max11945.bin:recovery/root/vendor/firmware/max11945.bin
